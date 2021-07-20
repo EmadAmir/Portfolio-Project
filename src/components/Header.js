91131c53381e4c505654ca27d59c2f7fd0b595f3
Emad <24242.imad@gmail.com>
2021-07-21 00:08:20 +0100

    completed the header

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+//rafce + tap
+import React from 'react'
+import Typed from "react-typed";
+
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className = "main-info">
+               <h1>Emad Sanaulla Amir</h1>
+               <Typed
+               className="typed-text"
+               strings={["Web Design","Web Developer", "Front-End Developer", "C# Developer"]}
+               typeSpeed={30}
+               backSpeed={50}
+               loop
+               /> 
+               <a href="/#" className="btn-main-offer">Contact Me</a>
+            </div>
+            
+        </div>
+
+    )
+}
+
+export default Header
